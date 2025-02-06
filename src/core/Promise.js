f9d14381aeece2e52dea47ecae491be98a6d23eb
haseeb hijazi <haseebk841@gmail.com>
2025-02-06 15:53:35 +0530

    executor methods built

--- src/core/Promise.js
+++ src/core/Promise.js
@@ -2,10 +2,31 @@ import { STATE } from "../utils/enums/states";
 import { FATE } from "../utils/enums/fates";
 
 class Promise {
+  #state;
+  #fate;
+  #value;
+  #reason;
   constructor(executor) {
-    this.state = STATE.PENDING;
-    this.fate = FATE.UNRESOLVED;
-    this.value = undefined;
-    this.reason = undefined;
+    this.#state = STATE.PENDING;
+    this.#fate = FATE.UNRESOLVED;
+    this.#value = undefined;
+    this.#reason = undefined;
+
+    try {
+      executor(this.#resolve, this.#reject);
+    } catch (error) {
+      this.#reject();
+    }
+  }
+
+  #resolve(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#state = STATE.FULFILLED;
+    this.#value = value;
+  }
+  #reject(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#state = STATE.REJECTED;
+    this.#value = value;
   }
 }
